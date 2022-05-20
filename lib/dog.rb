8cf6ec50ece524dd73c2d790a631642322c2d36e
Amer Ibric <ameribric@gmail.com>
2022-05-20 16:56:41 -0400

    commit tree 74ecc893e23b7540f078b5b72b3fc7f089bd4677

---
+++ lib/dog.rb
@@ -0,0 +1,37 @@
+
+
+class Dog 
+    attr_accessor :breed, :name
+    def initialize(breed="Mutt", name)
+        @breed = breed
+        @name = name
+    end
+end
+
+
+
+
+# class Dog
+#     attr_accessor :breed
+#   end
+  
+#   snoopy = Dog.new
+
+
+#   snoopy = Dog.new
+#   snoopy.breed = "Biggie"
+
+
+#   Let's define an #initialize 
+#   method that takes in an argument of a dog's breed
+#   and sets a @breed variable equal to that argument
+# # this below is doing what above is asking for 
+#   class Dog
+#     attr_reader :breed
+  
+#     def initialize(breed)
+#       @breed = breed
+#     end
+  
+#   end
+
